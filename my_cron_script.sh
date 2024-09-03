b258aebba7fb829da767dc6f4f293d6af1c2f732
yuebinghui <yb19834404044@sina.com>
2024-09-03 08:39:36 +0800

    2024年 9月 3日 星期二 08时39分36秒 CST

--- my_cron_script.sh
+++ my_cron_script.sh
@@ -5,6 +5,7 @@ source /etc/profile
 cd /Users/yuebinghui/Documents/program/github/note          
 git add .
 git commit -m "$(date)"
+git push
 
 echo "当前时间：$(date)" >> /Users/yuebinghui/Documents/program/github/note/time.txt
 
